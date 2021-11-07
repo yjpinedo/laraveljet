b1b5e717cc3651c8bb88103062b56465871edce2
Yeimer Piendo <yjpinedo@unicesar.edu.co>
2021-11-06 21:49:22 -0500

    Rutas

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,34 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/texto', function () {
+    return 'Este es un Texto';
+});
+
+Route::get('/arreglo', function () {
+    return [1, 2, ['array'], 'holaMundo'];
+});
+
+Route::get('/nombre/{nombre}', function ($nombre) {
+    return '<h1>El nombre es:' . $nombre . '</h1>';
+});
+
+Route::get('/nombre1/{nombre}', function ($nombre) {
+    return "<h1>El nombre es: $nombre</h1>";
+});
+
+Route::get('/cliente/{cliente?}', function($cliente = 'Cliente General'){
+    return "<h1>El cliente es: $cliente </h1>";
+});
+
+Route ::get('/ruta1', function(){
+    return '<h1>Esta es la vista de la Ruta 1</h1>';
+})->name('RutaNro1');
+
+Route ::get('/ruta2', function(){
+    return redirect()->route('RutaNro1');
+});
+
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
